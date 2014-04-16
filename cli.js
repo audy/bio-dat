a7a8bbea0c3deb53625cd3a87d0a836694f47fa1
Austin Davis-Richardson <harekrishna@gmail.com>
2014-04-16 09:34:42 -0400

    added -n option to specify number of sequences

--- cli.js
+++ cli.js
@@ -6,6 +6,9 @@ var argv = minimist(process.argv.slice(2));
 
 var cmd = argv._[0];
 
+// number of sequences to print
+var n_seqs = argv['n'];
+
 // generate a random nucleotide sequence of length length.
 function random_sequence(length) {
   var sequence = "";
@@ -19,10 +22,8 @@ function random_sequence(length) {
 switch(cmd) {
   case "seqs":
     console.log('i,sequence');
-    var i = 0;
-    while (true) {
+    for (var i = 0; i < n_seqs + 1; i++) {
       console.log(i + ',' + random_sequence(100));
-      i++;
     }
     break
 }
